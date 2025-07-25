d304742281d60127e1dcb297c0c272139e71ab00
Pablo <pablobau20@gmail.com>
2025-07-25 17:55:08 -0600

    Corrección: Restaurar el código de estado HTTP correcto

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def view_post(post_id):
     """Vista individual de un post"""
     post = blog_storage.get_post_by_id(post_id)
     if not post:
-        return render_template('404.html'), 500
+        return render_template('404.html'), 404
     return render_template('post.html', post=post, title=f'{post.title} - DevBlog')
 
 
